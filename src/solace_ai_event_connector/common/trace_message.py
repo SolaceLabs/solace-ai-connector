0fcae8324516ef28239b7c830d16732887fbb220
Edward Funnekotter <efunneko@gmail.com>
2024-04-27 19:37:05 -0400

    Allow-a-source_value-to-be-an-object (#5)
    * Remove launch.json
    
    * Merge remote-tracking branch 'upstream/main'
    
    * Allow a source_value in a transform to be an object in the config file

--- src/solace_ai_event_connector/common/trace_message.py
+++ src/solace_ai_event_connector/common/trace_message.py
@@ -2,10 +2,10 @@
 
 
 class TraceMessage:
-    def __init__(self, message, location, type="Trace"):
+    def __init__(self, message, location, trace_type="Trace"):
         self.message = message
         self.location = location
-        self.type = type
+        self.trace_type = trace_type
 
     def __str__(self):
-        return f"{self.type} at {self.location}\n{self.message}\n"
+        return f"{self.trace_type} at {self.location}\n{self.message}\n"
