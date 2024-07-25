c6c97ecae40f1fe31968b1c18742320102f7092a
Edward Funnekotter <efunneko@gmail.com>
2024-07-25 15:01:12 -0400

    Set the log level to the min between STDOUT and file

--- src/solace_ai_connector/common/log.py
+++ src/solace_ai_connector/common/log.py
@@ -5,26 +5,25 @@ import logging
 import logging.handlers
 
 
-log = logging.getLogger("mylog")
-log.setLevel(logging.DEBUG)
+log = logging.getLogger("solace_ai_connector")
+
 
 # Function to setup the configuration for the logger
 def setup_log(logFilePath, stdOutLogLevel, fileLogLevel):
+    # Set the global logger level to the lowest of the two levels
+    log.setLevel(min(stdOutLogLevel, fileLogLevel))
+
     stream_handler = logging.StreamHandler(sys.stdout)
     stream_handler.setLevel(stdOutLogLevel)
-    stream_formatter = logging.Formatter(
-        '%(message)s')
+    stream_formatter = logging.Formatter("%(message)s")
     stream_handler.setFormatter(stream_formatter)
 
-    #file_handler = logging.handlers.TimedRotatingFileHandler(
+    # file_handler = logging.handlers.TimedRotatingFileHandler(
     #    filename=logFilePath, when='midnight', backupCount=30, mode='w')
-    file_handler = logging.FileHandler(
-        filename=logFilePath, mode='w')
-    file_formatter = logging.Formatter(
-        '%(asctime)s |  %(levelname)s: %(message)s')
+    file_handler = logging.FileHandler(filename=logFilePath, mode="w")
+    file_formatter = logging.Formatter("%(asctime)s |  %(levelname)s: %(message)s")
     file_handler.setFormatter(file_formatter)
     file_handler.setLevel(fileLogLevel)
 
     log.addHandler(file_handler)
     log.addHandler(stream_handler)
-
