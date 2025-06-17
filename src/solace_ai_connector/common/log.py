7a1d5e637c27b52707dff0c29fad407eb9b5ef27
Hugo Pare <hugo.pare@solace.com>
2025-06-17 14:18:01 -0400

    Omitting recommendations

--- src/solace_ai_connector/common/log.py
+++ src/solace_ai_connector/common/log.py
@@ -74,23 +74,18 @@ def _format_with_trace(message, trace):
 # Trace is conditionally enabled based on the enableTrace parameter.
 _MODULE_ORIGINAL_DEBUG = log.debug
 _MODULE_ORIGINAL_ERROR = log.error
-_TRACE_ENABLED = False  # Global flag to track trace enablement
-
-def _update_trace_enabled(enabled):
-    global _TRACE_ENABLED
-    _TRACE_ENABLED = enabled
 
 def _create_module_wrapper_with_trace(original_method):
     def wrapper(message, *args, trace=None, **kwargs):
         if args and isinstance(message, str):
             formatted_message = message % args if args else message
-            if trace and _TRACE_ENABLED:
+            if trace:
                 full_message = _format_with_trace(formatted_message, trace)
             else:
                 full_message = formatted_message
             original_method(full_message, stacklevel=2, **kwargs)
         else:
-            if trace and _TRACE_ENABLED:
+            if trace:
                 full_message = _format_with_trace(message, trace)
             else:
                 full_message = message
